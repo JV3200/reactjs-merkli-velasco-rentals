49e5e1e7a80042cf81ea1830e0383d05228e7e5d
jv3200 <johnnyv3200@gmail.com>
2023-07-24 21:50:14 -0700

    updated readme and office studio requirments

--- src/pages/OfficeStudios.jsx
+++ src/pages/OfficeStudios.jsx
@@ -4,6 +4,14 @@ function OfficeStudios() {
   return (
     <div className="officeStudios">
       <h1>OfficeStudios</h1>
+      <h2>
+        <ul>
+          <li>Studio Name</li>
+          <li>Studio picture</li>
+          <li>Studio website</li>
+          <li>available spaces</li>
+        </ul>
+      </h2>
     </div>
   );
 }
