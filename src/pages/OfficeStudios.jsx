e48c62e652d3c8b6db682f6b8731a681bb3a858b
jv3200 <johnnyv3200@gmail.com>
2023-08-07 22:59:48 -0700

    added website links to some cards

--- src/pages/OfficeStudios.jsx
+++ src/pages/OfficeStudios.jsx
@@ -21,6 +21,7 @@ function OfficeStudios() {
           title="Shonto Begay Art Studio"
           imageUrl="../assets/images/Red-rocks.jpg"
           description="Shonto Begay's Art Studio in downtown Flagstaff, Arizona, welcomes you to a captivating space where vibrant paintings, intricate illustrations, and rich Navajo storytelling come to life. Explore the artist's mastery of color and form, gaining insight into the essence of Navajo culture through his art and acclaimed books. This warm and inviting studio offers a profound encounter with Shonto Begay's creative brilliance and the timeless beauty of Native American heritage."
+          websiteUrl="https://shontobegay.net/"
         />
         <Cards />
       </div>
