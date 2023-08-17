38a09e3d054754a5fc1bc2a641a6957f1a2598ec
jv3200 <johnnyv3200@gmail.com>
2023-08-16 22:30:39 -0700

    Added store front page websites and card image

--- src/pages/Storefronts.jsx
+++ src/pages/Storefronts.jsx
@@ -3,6 +3,10 @@ import Cards from "../components/Cards";
 import "../App.css";
 import shaneimg from "../assets/images/Shane-knight.jpeg";
 import barber from "../assets/images/Barber.jpeg";
+import cafe from "../assets/images/Cafe.png";
+import incahoots from "../assets/images/Incahoots.jpeg";
+import rooster from "../assets/images/rooster_bus.jpeg";
+import yogaert from "../assets/images/yogart-2.jpeg";
 
 function Storefronts() {
   return (
@@ -12,17 +16,25 @@ function Storefronts() {
       <div className="storefronts">
         <Cards
           title="Barber"
-          src={barber}
+          img={barber}
           description="Welcome to Dluxe Barber Shop, a premier grooming destination in downtown Flagstaff. Step inside our stylish and modern space, where expert barbers provide top-notch haircuts, shaves, and grooming services. Enjoy a relaxed and welcoming atmosphere while experiencing the artistry of our skilled team. Discover a new level of sophistication and confidence at Dluxe Barber Shop, where every visit promises a dapper transformation."
           websiteUrl="https://downtownflagstaff.org/go/dluxe-barber-shop"
         />
-        <Cards />
-        <Cards />
-        <Cards />
-        <Cards />
+        <Cards
+          title="Yogaert"
+          img={yogaert}
+          websiteUrl="https://www.instagram.com/yogaert/"
+        />
+        <Cards
+          title="Rooster Bus"
+          img={rooster}
+          websiteUrl="https://shoptherooster.com/"
+        />
+        <Cards title="Incahoots" img={incahoots} />
+        <Cards title="Cafe Stella" img={cafe} />
         <Cards
           title="Shane Knight Photography"
-          src={shaneimg}
+          img={shaneimg}
           description="Specializing in fine made photography of western horses and the beautiful landscapes that surround them. Offering Beautiful Printed and Framed Fine Art Photography Since 1998"
           websiteUrl="http://shaneknight.com/"
         />
