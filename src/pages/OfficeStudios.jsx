74e1637463d3f175c0e9c9fa371d408b0b7e7333
jv3200 <johnnyv3200@gmail.com>
2023-08-22 22:51:32 -0700

    added some images to officeStudios page

--- src/pages/OfficeStudios.jsx
+++ src/pages/OfficeStudios.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Cards from "../components/Cards";
 import "../App.css";
+import joe from "../assets/images/JoeSorren-trading-cards.jpeg";
+import shonto from "../assets/images/shonto-painting.jpeg";
 
 function OfficeStudios() {
   return (
@@ -10,16 +12,16 @@ function OfficeStudios() {
       <div className="storefronts">
         <Cards
           title="Joe Soren"
-          src="../assets/images/historic-downtown-flagstaff.jpg"
+          img={joe}
           description="Discover the enchanting world of Joe Sorren's Art Studio in downtown Flagstaff, AZ. Step into a sanctuary of dreamlike paintings, evoking emotions and sparking imagination. Surreal landscapes and expressive figures invite you on an introspective journey. Embrace the captivating allure of Sorren's art—a tribute to the boundless magic of creativity."
         />
-        <Cards />
-        <Cards />
-        <Cards />
-        <Cards />
+        <Cards title="" src="" description="" websiteUrl="" />
+        <Cards title="" src="" description="" websiteUrl="" />
+        <Cards title="" src="" description="" websiteUrl="" />
+        <Cards title="" src="" description="" websiteUrl="" />
         <Cards
           title="Shonto Begay Art Studio"
-          imageUrl="../assets/images/Red-rocks.jpg"
+          img={shonto}
           description="Shonto Begay's Art Studio in downtown Flagstaff, Arizona, welcomes you to a captivating space where vibrant paintings, intricate illustrations, and rich Navajo storytelling come to life. Explore the artist's mastery of color and form, gaining insight into the essence of Navajo culture through his art and acclaimed books. This warm and inviting studio offers a profound encounter with Shonto Begay's creative brilliance and the timeless beauty of Native American heritage."
           websiteUrl="https://shontobegay.net/"
         />
